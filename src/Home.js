f02a249de1bbbe18c36d6786534aa1c3e865c7aa
unknown <ppraveenaperiyasamy@gmail.com>
2023-04-15 14:53:00 +0530

    home changes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick= (e) =>{
+        console.log('hello' ,e);
+    }
+    const handleClickAgain=(name)=>{
+        console.log('hi' + name);
+    }
     return ( 
     <div className="home">
         <h2>homepage</h2>
+        <button onClick={handleClick}>Click Me!</button>
+        <button onClick={()=>{
+            handleClickAgain('praveena')
+        }}>Click Me!</button>
     </div>
      );
 }
