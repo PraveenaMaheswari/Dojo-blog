20cdb6aaa8e5d5b5ec9bad866b19f5b465cf17c0
unknown <ppraveenaperiyasamy@gmail.com>
2023-04-15 21:22:06 +0530

    blog-preview

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
-    const handleClick= (e) =>{
-        console.log('hello' ,e);
-    }
-    const handleClickAgain=(name)=>{
-        console.log('hi' + name);
-    }
+import {useState} from 'react';
+
+const Home = () => { 
+    const[blogs,setBlogs]=useState([
+        {title:'The webpage', body:'The webpage i have developed...', author:'Praveena',id:1},
+        {title:'Techieguy', body:'The story of getting...', author:'Pranesh',id:2},
+        {title:'History', body:'Boring life has sucked me...', author:'Vinoth',id:3}
+    ]);
     return ( 
     <div className="home">
-        <h2>homepage</h2>
-        <button onClick={handleClick}>Click Me!</button>
-        <button onClick={()=>{
-            handleClickAgain('praveena')
-        }}>Click Me!</button>
+     {blogs.map((blog) =>(
+        <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+        </div>
+     ))}  
+        
+        
     </div>
      );
 }
